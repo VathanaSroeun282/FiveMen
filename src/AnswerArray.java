c9c7e48490d9ad33151a4bbabc1e36c56af3392c
VathanaKung357 <vathanakkung14@gmail.com>
2023-12-06 20:41:02 +0700

    Answer Array

---
+++ src/AnswerArray.java
@@ -0,0 +1,17 @@
+public class AnswerArray
+{
+    public static void main(String[] args) {
+        String [][] a = {
+                {"-","-","-","-","-","-","-","-","-","-"},
+                {"-","-","-","-","-","-","-","-","-","-"},
+                {"-","-","-","-","-","-","-","-","-","-"},
+                {"-","-","-","-","-","-","-","-","-","-"}
+        };
+        for (int i = 0; i < 10 ; i++ ) {
+            for(int j = 0 ; j < 10 ; j ++){
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
